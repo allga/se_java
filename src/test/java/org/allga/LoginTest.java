b1f1932c0cf6d159d939e639ea5e929da61bfb7a
Olga Borovyk <pennerall@gmail.com>
2016-03-20 22:22:20 +0200

    Imports optimized in LoginTest

--- src/test/java/org/allga/LoginTest.java
+++ src/test/java/org/allga/LoginTest.java
@@ -1,21 +1,16 @@
 package org.allga;
 
-import java.util.regex.Pattern;
-import java.util.concurrent.TimeUnit;
-import org.testng.*;
-import org.testng.annotations.*;
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
-import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.testng.annotations.Test;
 
 public class LoginTest extends TestBase {
     private boolean acceptNextAlert = true;
     private StringBuffer verificationErrors = new StringBuffer();
 
     @Test
-    public void testUntitled2() throws Exception {    driver.get(baseUrl + "/php4dvd/");
+    public void testUntitled2() throws Exception {    driver.get(baseUrl + "php4dvd/");
         driver.findElement(By.id("username")).clear();
         driver.findElement(By.id("username")).sendKeys("admin");
         driver.findElement(By.name("password")).clear();
