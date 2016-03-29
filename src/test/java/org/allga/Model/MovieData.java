f26751a5a1b5c284bf753a4b9e4a06533d7d9849
Olga Borovyk <pennerall@gmail.com>
2016-03-29 13:11:51 +0300

    class MovieData created

---
+++ src/test/java/org/allga/Model/MovieData.java
@@ -0,0 +1,251 @@
+package org.allga.model;
+
+/**
+ * Created by Olga on 29.03.2016.
+ */
+public class MovieData {
+    private String imdbID;
+    private int number;
+    private String title;
+    private String knownAs;
+    private int year;
+    private int duration;
+    private int rating;
+    private String format;
+    private boolean ownMovie;
+    private boolean alreadySeen;
+    private boolean loanedOut;
+    private String trailerUrl;
+    private String personalNotes;
+    private String taglines;
+    private String plotOutline;
+    private String plots;
+    private String languages;
+    private String subtitles;
+    private String audio;
+    private String video;
+    private String country;
+    private String genres;
+    private String director;
+    private String writer;
+    private String producer;
+    private String music;
+    private String cast;
+
+    public String getImdbID() {
+        return imdbID;
+    }
+
+    public void setImdbID(String imdbID) {
+        this.imdbID = imdbID;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getKnownAs() {
+        return knownAs;
+    }
+
+    public void setKnownAs(String knownAs) {
+        this.knownAs = knownAs;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public void setFormat(String format) {
+        this.format = format;
+    }
+
+    public boolean isOwnMovie() {
+        return ownMovie;
+    }
+
+    public void setOwnMovie(boolean ownMovie) {
+        this.ownMovie = ownMovie;
+    }
+
+    public boolean isAlreadySeen() {
+        return alreadySeen;
+    }
+
+    public void setAlreadySeen(boolean alreadySeen) {
+        this.alreadySeen = alreadySeen;
+    }
+
+    public boolean isLoanedOut() {
+        return loanedOut;
+    }
+
+    public void setLoanedOut(boolean loanedOut) {
+        this.loanedOut = loanedOut;
+    }
+
+    public String getTrailerUrl() {
+        return trailerUrl;
+    }
+
+    public void setTrailerUrl(String trailerUrl) {
+        this.trailerUrl = trailerUrl;
+    }
+
+    public String getPersonalNotes() {
+        return personalNotes;
+    }
+
+    public void setPersonalNotes(String personalNotes) {
+        this.personalNotes = personalNotes;
+    }
+
+    public String getTaglines() {
+        return taglines;
+    }
+
+    public void setTaglines(String taglines) {
+        this.taglines = taglines;
+    }
+
+    public String getPlotOutline() {
+        return plotOutline;
+    }
+
+    public void setPlotOutline(String plotOutline) {
+        this.plotOutline = plotOutline;
+    }
+
+    public String getPlots() {
+        return plots;
+    }
+
+    public void setPlots(String plots) {
+        this.plots = plots;
+    }
+
+    public String getLanguages() {
+        return languages;
+    }
+
+    public void setLanguages(String languages) {
+        this.languages = languages;
+    }
+
+    public String getSubtitles() {
+        return subtitles;
+    }
+
+    public void setSubtitles(String subtitles) {
+        this.subtitles = subtitles;
+    }
+
+    public String getAudio() {
+        return audio;
+    }
+
+    public void setAudio(String audio) {
+        this.audio = audio;
+    }
+
+    public String getVideo() {
+        return video;
+    }
+
+    public void setVideo(String video) {
+        this.video = video;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getGenres() {
+        return genres;
+    }
+
+    public void setGenres(String genres) {
+        this.genres = genres;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+
+    public String getMusic() {
+        return music;
+    }
+
+    public void setMusic(String music) {
+        this.music = music;
+    }
+
+    public String getCast() {
+        return cast;
+    }
+
+    public void setCast(String cast) {
+        this.cast = cast;
+    }
+
+}
