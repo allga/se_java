2e7a7b70d456b6203a5f1ccd95df80525b502a25
Olga Borovyk <pennerall@gmail.com>
2016-03-29 20:45:35 +0300

    MovieDeletionTests created

---
+++ src/test/java/org/allga/tests/MovieDeletionTests.java
@@ -0,0 +1,29 @@
+package org.allga.tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Created by Olga on 29.03.2016.
+ */
+public class MovieDeletionTests extends TestBase {
+
+    @Test
+    public void testDeletionMovie() {
+        login("admin", "admin");
+        List<WebElement> before = new ArrayList<WebElement>(driver.findElements(By.cssSelector("div.movie_box")));
+        before.get(0).click();
+        click(By.xpath("//*[@class='button']//a[.//img[@title=\"Remove\"]]"));
+        driver.switchTo().alert().accept();
+        List<WebElement> after = new ArrayList<WebElement>(driver.findElements(By.cssSelector("a[href*=\"go=movie&id\"]")));
+        assertThat(after.size(), equalTo(before.size() - 1));
+    }
+
+}
