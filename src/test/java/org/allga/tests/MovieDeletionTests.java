34f9e93cc58ae47992d4c6ec3d2ce3be78e82866
Olga Borovyk <pennerall@gmail.com>
2016-03-30 17:50:21 +0300

    MovieDeletionTests corrected

--- src/test/java/org/allga/tests/MovieDeletionTests.java
+++ src/test/java/org/allga/tests/MovieDeletionTests.java
@@ -1,11 +1,6 @@
 package org.allga.tests;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -17,12 +12,13 @@ public class MovieDeletionTests extends TestBase {
 
     @Test
     public void testDeletionMovie()  {
-        List<WebElement> before = new ArrayList<WebElement>(driver.findElements(By.cssSelector("div.movie_box")));
-        before.get(0).click();
-        getHelperBase().click(By.xpath("//*[@class='button']//a[.//img[@title=\"Remove\"]]"));
-        driver.switchTo().alert().accept();
-        List<WebElement> after = new ArrayList<WebElement>(driver.findElements(By.cssSelector("a[href*=\"go=movie&id\"]")));
-        assertThat(after.size(), equalTo(before.size() - 1));
+        getNavigationHelper().goToHomePage();
+        int before = getMovieHelper().countAllMovies();
+        getMovieHelper().allMovies().get(0).click();
+        getMovieHelper().deleteMovie();
+        getNavigationHelper().goToHomePage();
+        int after = getMovieHelper().countAllMovies();
+        assertThat(after, equalTo(before - 1));
     }
 
 }
