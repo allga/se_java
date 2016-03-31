49f051a39b5d7e8902a47855c0f8f6a1c52b65a3
Olga Borovyk <pennerall@gmail.com>
2016-03-31 21:01:33 +0300

    in MovieCreationTests comparing corrected with Lists

--- src/test/java/org/allga/tests/MovieCreationTests.java
+++ src/test/java/org/allga/tests/MovieCreationTests.java
@@ -3,6 +3,9 @@ package org.allga.tests;
 import org.allga.model.MovieData;
 import org.testng.annotations.Test;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -14,35 +17,41 @@ public class MovieCreationTests extends TestBase {
 
     @Test
     public void testPositiveCreationMovie() {
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
         getNavigationHelper().goToHomePage();
-        int before = getMovieHelper().countAllMovies();
+        List<MovieData> moviesBefore = getMovieHelper().getAllMovies();
 
         getNavigationHelper().goToAddMoviePage();
         MovieData movie = new MovieData().setNumber("0711111").setTitle("Miles Ahead").setYear("2015").setDuration("100");
         getMovieHelper().fillMovieForm(movie);
         getMovieHelper().submitCreationMovie();
+
         assertThat((driver.getCurrentUrl()), containsString("php4dvd/?go=movie&id="));
 
         getNavigationHelper().goToHomePage();
-        int after = getMovieHelper().countAllMovies();
-        assertThat(after, equalTo(before + 1));
+        List<MovieData> moviesAfter = getMovieHelper().getAllMovies();
+
+        assertThat(moviesAfter.size(), equalTo(moviesBefore.size() + 1));
 
     }
 
     @Test
     public void testRequiredFieldCreationMovie() {
         getNavigationHelper().goToHomePage();
-        int before = getMovieHelper().countAllMovies();
+        List<MovieData> moviesBefore = getMovieHelper().getAllMovies();
 
         getNavigationHelper().goToAddMoviePage();
         MovieData movie = new MovieData().setNumber("0711111").setYear("2015").setDuration("100");
         getMovieHelper().fillMovieForm(movie);
         getMovieHelper().submitCreationMovie();
+
         assertThat((driver.getCurrentUrl()), containsString("php4dvd/?go=add"));
 
         getNavigationHelper().goToHomePage();
-        int after = getMovieHelper().countAllMovies();
-        assertThat(after, equalTo(before));
+        List<MovieData> moviesAfter = getMovieHelper().getAllMovies();
+
+        assertThat(moviesAfter.size(), equalTo(moviesBefore.size()));
     }
 
 }
